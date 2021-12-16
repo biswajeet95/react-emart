6fdb662e8875b2e6e41798e51e38487da6b2597c
Biswajeet moharana <83495994+biswajeet95@users.noreply.github.com>
2021-12-16 13:59:16 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
-///For Add Item to Cart
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/font-awesome/css/font-awesome.min.css';
+import {BrowserRouter } from 'react-router-dom';
+import {Provider} from 'react-redux';
+import store from './redux/store'
 
-export const addCart = (product) =>{
-    return{
-        type : "ADDITEM",
-        payload : product
-    }
-}
 
-//For Delete Item   For Cart
-export const deCart = (product) => {
-    return{
-        type:"DELITEM" ,
-        payload : product
-    }
-}
+ReactDOM.render(
+  <BrowserRouter>
+  <Provider store={store}><App/></Provider>
+   
+  </BrowserRouter>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
